eeca18c81b06aaf7584cc847c18f3fe26196269d
jokerpan <pan961791953@qq.com>
2017-09-09 11:37:09 +0800

    修改导航航栏

--- js/nav.js
+++ js/nav.js
@@ -23,19 +23,8 @@ $(function(){
 	$('nav ul li a').hover(function() {
 		selected.removeClass('nav-active');
 		$(this).parents('li').addClass('nav-active');
-		$(this).css('color', '#fff');
-		if ($(this).parents('li') != $('nav ul li:eq(1)'))
-		{
-			$(this).children('img').addClass('img-invert');
-		}
 	}, function() {
 		$(this).parents('li').removeClass('nav-active');
-		$(this).filter('a').css('color', '#777');
-		if ($(this).parents('li') == $('nav ul li:eq(1)')) 
-		{
-			$('nav ul li:eq(1)').children('img').addClass('img-invert');
-		}
-		$(this).children('img').removeClass('img-invert');
 		selected.addClass('nav-active');
 	});
 })
